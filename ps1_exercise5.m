52a6d7ebb39b61cfa210b3650cfb5ea6b89bf730
Loha Hashimy <loha_hashimy@yahoo.com>
2016-04-24 07:37:46 -1100

    Delete ps1_exercise5.m

--- ps1_exercise5.m
+++
@@ -1,5 +0,0 @@
-function theta_hat = ps1_exercise5(data,a0)
-  y = @(x) -loglik(x,data); % finding the maximum using fminunc means taking
-                            % the opposite sign of the function
-  theta_hat = fminunc(y,a0);
-end
